ee4ae7eedcbd85682927031bdf61fdb33a0f2d88
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-03 10:47:49 +0600

    :fire: .env

--- src/firebase/Firebase.init.js
+++ src/firebase/Firebase.init.js
@@ -4,21 +4,19 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgjMt6L9JE36EsWxD-ihDWCxHSfpzg7m4",
-  authDomain: "login-registration-proje-53fc4.firebaseapp.com",
-  projectId: "login-registration-proje-53fc4",
-  storageBucket: "login-registration-proje-53fc4.appspot.com",
-  messagingSenderId: "727544154483",
-  appId: "1:727544154483:web:7643a5774db21349ac9d06",
-  //   apiKey: process.env.REACT_APP_API_KEY,
-  //   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  //   projectId: process.env.REACT_APP_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  //   appId: process.env.REACT_APP_APP_ID,
+  
+  apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+
 };
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
