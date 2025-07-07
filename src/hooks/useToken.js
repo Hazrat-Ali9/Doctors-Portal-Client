1a2cbcfdbb7677a34da5aaafeb3d50abdac8fb8d
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-07 15:23:24 +0600

    user token 🛫

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+// user token
 const useToken = (user) => {
     const [token, setToken] = useState('');
 
